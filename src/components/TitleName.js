df48e6a3513c46c48d591eabb2c789a740dba754
rikito_rakib <rikitoaf@gmail.com>
2024-08-13 18:58:59 +0600

    updated4

--- src/components/TitleName.js
+++ src/components/TitleName.js
@@ -2,9 +2,66 @@
 
 const TitleName = [
     '',
-    'Stage',
-    'Table',
+    'Basin',
+    'Backdrop',
+    'Benchdrop',
+    'Car',
+    'Cador Frame',
+    'Center Piece',
+    'Celling',
+    'Cinematography',
     'Chair',
+    'Chain',
+    'DJ',
+
+    'Entry Gate',
+    'Food Stall',
+    'Flower Chandan',
+    'Glass Ramp',
+    'Hall Setup',
+    'Kungo',
+    'LED Screen',
+    'Line Camera',
+    'Lounge',
+    'Mala',
+    'Palki',
+    'PVC',
+    'Photography',
+    'Photo Booth',
+    'Pyro Light',
+   
+    'Platform',
+    
+    'Projector',
+
+    
+    'Side Screen',
+    'Sound System',
+    'Swing/Dolna',
+
+
+   
+    'Stage',
+  
+    
+    'Stair',
+    
+    'Table',
+    'Tent',
+    
+
+    
+    'Welcome Banner',
+  
+   
+    
+    
+   
+ 
+   
+    
+   
+
 ]
 
 export default TitleName
